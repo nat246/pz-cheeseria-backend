109132a276ab314581b0a7046483062049a6cedb
Nathanael Ang <natnatang@hotmail.com>
2024-05-02 19:26:23 +1000

    Refactor ProductsController and CheesesRepository to use mock data in unit tests

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -1,22 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
-import { Cheese } from './cheese.entity';
-import { CheeseColor } from './cheese-color.enum';
 import { CheesesRepository } from './cheeses.repository';
+import { CheeseColor } from './cheese-color.enum';
+import { Cheese } from './cheese.entity';
+
+const mockCheesesRepository = () => ({
+  createCheese: jest.fn(),
+  find: jest.fn().mockReturnValueOnce([
+    {
+      id: 1,
+      name: 'Cheddar',
+      description: 'Pretty good',
+      image: 'https://example.com/cheddar.jpg',
+      price: 12.99,
+      color: CheeseColor.PALE_YELLOW,
+    },
+    {
+      id: 2,
+      name: 'Gouda',
+      description: "It's alright",
+      image: 'https://example.com/gouda.jpg',
+      price: 10.5,
+      color: CheeseColor.ORANGE,
+    },
+  ]),
+});
 
 describe('ProductsController', () => {
   let controller: ProductsController;
   let service: ProductsService;
+  let repository: CheesesRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProductsController],
-      providers: [ProductsService, CheesesRepository],
+      providers: [
+        ProductsService,
+        { provide: CheesesRepository, useFactory: mockCheesesRepository },
+      ],
     }).compile();
 
     controller = module.get<ProductsController>(ProductsController);
     service = module.get<ProductsService>(ProductsService);
+    repository = module.get<CheesesRepository>(CheesesRepository);
   });
 
   it('should be defined', () => {
